e6144abbb389207c2f3d2380cb844474533e83cf
Imtiaz Epu <imtiazepu@programmer.net>
2021-10-03 10:44:54 +0600

    route error handle with try& catch

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
 
 use Phroute\Phroute\Dispatcher;
+use Phroute\Phroute\Exception\HttpMethodNotAllowedException;
+use Phroute\Phroute\Exception\HttpRouteNotFoundException;
 use Phroute\Phroute\RouteCollector;
 use Phroute\Phroute\RouteParser;
 
 require_once "vendor/autoload.php";
 
-$router = new RouteCollector(new RouteParser());
+$router = new RouteCollector( new RouteParser() );
 
-$router->get('/',function (){
+$router->post( '/', function () {
 	return "Hello from PHRoute";
-});
+} );
 
-$dispatcher = new Dispatcher($router->getData());
+$dispatcher = new Dispatcher( $router->getData() );
 
-$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+try {
+	$response = $dispatcher->dispatch( $_SERVER['REQUEST_METHOD'], parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH ) );
+} catch ( HttpRouteNotFoundException $e ) {
+	echo $e->getMessage();
+	die();
+}catch (HttpMethodNotAllowedException $e){
+	echo $e->getMessage();
+	die();
+}
+
 
 echo $response;
